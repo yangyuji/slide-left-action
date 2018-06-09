521d57c6f9bc8dd86593f070c4ac2cebf4792047
yangyuji <394712273@qq.com>
2018-06-09 10:05:01 +0800

    update

--- slideLeftAction.js
+++ slideLeftAction.js
@@ -1,51 +1,38 @@
 /*
 * author: "oujizeng",
 * license: "MIT",
-* name: "moveToDelete.js",
-* version: "1.2.6"
+* name: "slideLeftAction.js",
+* version: "1.3.0"
 */
 
 (function (root, factory) {
     if (typeof module != 'undefined' && module.exports) {
         module.exports = factory();
     } else {
-        root['MoveToDelete'] = factory();
+        root['slideLeftAction'] = factory();
     }
 }(this, function () {
+    'use strict'
 
-    var getAll = function (str) {
-        return document.querySelectorAll(str);
-    };
+    var _transform = function (el, attr, val) {
+        var vendors = ['', 'Webkit', 'ms', 'Moz', 'O', 'Khtml'],
+            body = document.body || document.documentElement;
 
-    var util = {
-        hasClass: function (e, c) {
-            var re = new RegExp("(^|\\s)" + c + "(\\s|$)");
-            return re.test(e.className);
-        },
-        addClass: function (e, c) {
-            if ( this.hasClass(e, c) ) {
-                return;
+        [].forEach.call(vendors, function (vendor) {
+            var styleAttr = vendor ? vendor + attr : attr.charAt(0).toLowerCase() + attr.substr(1);
+            // console.log(styleAttr);
+            if (typeof body.style[styleAttr] === 'string') {
+                el.style[styleAttr] = val;
             }
-            var newclass = e.className.split(' ');
-            newclass.push(c);
-            e.className = newclass.join(' ');
-        },
-        removeClass: function (e, c) {
-            if ( !this.hasClass(e, c) ) {
-                return;
-            }
-            var re = new RegExp("(^|\\s)" + c + "(\\s|$)", 'g');
-            e.className = e.className.replace(re, '');
-        }
-    };
+        });
+    }
 
-    var MoveToDelete = {
+    var slideLeftAction = {
 
         init: function(opt){
 
-
             var moveCount = opt.moveCount || 80,            // 位移距离
-                container = getAll(opt.container);          // 主容器
+                container = document.querySelectorAll(opt.container);  // 主容器
 
             for (var i = 0; i < container.length; i++) {
 
@@ -57,29 +44,26 @@
                     var btn = document.createElement('div');
                     btn.textContent = opt.buttons[n].text;
                     btn.classList.add(opt.buttons[n].class);
+                    btn.addEventListener('click', opt.buttons[n].click || null, false);
                     container[i].appendChild(btn);
                 }
 
                 // 开启硬件加速
-                container[i].style.webkitTransform = 'translateZ(0)';
-                container[i].style.transform = 'translateZ(0)';
+                _transform(container[i], 'transform', 'translateZ(0)');
 
                 // 开始滑动
                 container[i].addEventListener('touchstart', function (event) {
 
                     // 还原动画时间
-                    this.style.webkitTransitionDuration = '0ms';
-                    this.style.transitionDuration = '0ms';
+                    _transform(this, 'transitionDuration', '0ms');
 
                     // 关闭其他项的按钮，也可以放在滑动结束
                     for (var ii = 0; ii < container.length; ii++) {
-                        if (util.hasClass(container[ii], 'move-out-click') && container[ii] != this) {
+                        if (container[ii].classList.contains('move-out-click') && container[ii] != this) {
                             // 动画慢一点，避免卡帧
-                            container[ii].style.webkitTransitionDuration = '325ms';
-                            container[ii].style.transitionDuration = '325ms';
-                            container[ii].style.webkitTransform = 'translateX(0px)';
-                            container[ii].style.transform = 'translateX(0px)';
-                            util.removeClass(container[ii], 'move-out-click');
+                            _transform(container[ii], 'transitionDuration', '325ms');
+                            _transform(container[ii], 'transform', 'translateX(0px)');
+                            container[ii].classList.remove('move-out-click');
                         }
                     }
 
@@ -109,19 +93,17 @@
                         // 向右滑动
                         if (moveX > 0) {
                             // 有按钮的时候才处理
-                            if(util.hasClass(this, 'move-out-click')) {
+                            if(this.classList.contains('move-out-click')) {
                                 var x = moveX > moveCount ? 0 : -moveCount + moveX;
-                                this.style.webkitTransform = 'translateX(' + x + 'px)';
-                                this.style.transform = 'translateX(' + x + 'px)';
+                                _transform(this, 'transform', 'translateX(' + x + 'px)');
                             }
                         }
                         // 向左滑动
                         if (moveX < 0) {
                             // 没有按钮的时候才处理
-                            if(!(util.hasClass(this, 'move-out-click'))) {
+                            if(!(this.classList.contains('move-out-click'))) {
                                 var x = Math.abs(moveX) > moveCount ? -moveCount : moveX;
-                                this.style.webkitTransform = 'translateX(' + x + 'px)';
-                                this.style.transform = 'translateX(' + x + 'px)';
+                                _transform(this, 'transform', 'translateX(' + x + 'px)');
                             }
                         }
                     }
@@ -137,30 +119,27 @@
                     // 规划代码：上下滑动距离达到或者超过容器高度就指示为滚动，不做处理
 
                     // 动画慢一点，避免卡帧
-                    this.style.webkitTransitionDuration = '125ms';
-                    this.style.transitionDuration = '125ms';
+                    _transform(this, 'transitionDuration', '125ms');
 
                     // 已经显示按钮
-                    if(util.hasClass(this, 'move-out-click')) {
+                    if(this.classList.contains('move-out-click')) {
 
                         // 向右滑动，超过位移系数一半就隐藏按钮
                         if(moveX > 0){
                             //var x = moveX > (moveCount / 2) ? 0 : -moveCount;
                             var x = moveX > 10 ? 0 : -moveCount;  //改为超过10就隐藏按钮
-                            this.style.webkitTransform = 'translateX(' + x + 'px)';
-                            this.style.transform = 'translateX(' + x + 'px)';
+                            _transform(this, 'transform', 'translateX(' + x + 'px)');
                             if (x === 0) {
-                                util.removeClass(this, 'move-out-click');
+                                this.classList.remove('move-out-click');
                             }
                         }
                     } else {
                         // 向左滑动，超过位移系数一半就显示按钮
                         if (moveX < 0) {
                             var x = Math.abs(moveX) > moveCount / 2 ? -moveCount : 0;
-                            this.style.webkitTransform = 'translateX(' + x + 'px)';
-                            this.style.transform = 'translateX(' + x + 'px)';
+                            _transform(this, 'transform', 'translateX(' + x + 'px)');
                             if (x !== 0 ) {
-                                util.addClass(this, 'move-out-click');
+                                this.classList.add('move-out-click');
                             }
                         }
                     }
@@ -175,11 +154,9 @@
                 container[i].addEventListener('touchcancel', function(event) {
                     console.log('cancel');
                     // 回到初始位置
-                    this.style.webkitTransitionDuration = '225ms';
-                    this.style.transitionDuration = '225ms';
-                    this.style.webkitTransform = 'translateX(0px)';
-                    this.style.transform = 'translateX(0px)';
-                    util.removeClass(this, 'move-out-click');
+                    _transform(this, 'transitionDuration', '225ms');
+                    _transform(this, 'transform', 'translateX(0px)');
+                    this.classList.remove('move-out-click');
                     // 恢复初始化状态
                     moveStart = null;
                     moveX = 0;
@@ -189,5 +166,5 @@
         }
     };
 
-    return MoveToDelete;
+    return slideLeftAction;
 }));
