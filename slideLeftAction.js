2112503833042f5fc2070b60b52a5678b4cb5de1
yangyuji <394712273@qq.com>
2018-10-12 10:35:47 +0800

    v1.3.3

--- slideLeftAction.js
+++ slideLeftAction.js
@@ -3,7 +3,7 @@
  * license: "MIT",
  * github: "https://github.com/yangyuji/slide-left-action",
  * name: "slideLeftAction.js",
- * version: "1.3.2"
+ * version: "1.3.3"
 */
 
 (function (root, factory) {
@@ -23,19 +23,16 @@
         this.moveCount = opt.moveCount || 80;
         this.container = document.querySelectorAll(opt.container);
         this.buttons = opt.buttons;
+        this.moveStart = null;
+        this.moveX = 0;
+        this.moveY = 0;
         this.init();
     }
 
     slideLeftAction.prototype = {
-        version: '1.3.2',
+        version: '1.3.3',
         init: function () {
-            var that = this;
-
             for (var i = 0; i < this.container.length; i++) {
-
-                var moveX = 0, moveY = 0,                   // 拖动量
-                    moveStart = null;                       // 开始抓取标志位
-
                 // 补充操作按钮
                 for (var n = 0; n < this.buttons.length; n++) {
                     var btn = document.createElement('div');
@@ -44,124 +41,124 @@
                     btn.addEventListener('click', this.buttons[n].click || null, false);
                     this.container[i].appendChild(btn);
                 }
-
                 // 开启硬件加速
-                util._transform(this.container[i], 'transform', 'translateZ(0)');
-
-                // 开始滑动
-                this.container[i].addEventListener('touchstart', function (event) {
-
-                    // 还原动画时间
-                    util._transform(this, 'transitionDuration', '0ms');
-
-                    // 关闭其他项的按钮，也可以放在滑动结束
-                    for (var ii = 0; ii < that.container.length; ii++) {
-                        if (that.container[ii].classList.contains('move-out-click') && that.container[ii] != this) {
-                            // 动画慢一点
-                            util._transform(that.container[ii], 'transitionDuration', '225ms');
-                            util._transform(that.container[ii], 'transform', 'translateX(0px)');
-                            that.container[ii].classList.remove('move-out-click');
-                        }
-                    }
-
-                    // 记录滑动位置
-                    moveStart = {
-                        x: event.touches[0].pageX,
-                        y: event.touches[0].pageY
-                    }
-                }, false);
-
-                // 滑动过程
-                this.container[i].addEventListener('touchmove', function (event) {
-                    // 防止手机滑动跨越了几个容器，造成没有开始位置
-                    if (moveStart === null) {
-                        return;
-                    }
-
-                    var nowX = event.touches[0].pageX;
-                    var nowY = event.touches[0].pageY;
-                    moveX = nowX - moveStart.x;
-                    moveY = nowY - moveStart.y;
-
-                    // 规划代码：上下滑动距离达到或者超过容器高度就指示为滚动，不做处理
-
-                    // 左右滑动
-                    if (Math.abs(moveX) > Math.abs(moveY)) {
-                        // 向右滑动
-                        if (moveX > 0) {
-                            // 有按钮的时候才处理
-                            if (this.classList.contains('move-out-click')) {
-                                var x = moveX > that.moveCount ? 0 : -that.moveCount + moveX;
-                                util._transform(this, 'transform', 'translateX(' + x + 'px)');
-                            }
-                        }
-                        // 向左滑动
-                        if (moveX < 0) {
-                            // 没有按钮的时候才处理
-                            if (!(this.classList.contains('move-out-click'))) {
-                                var x = Math.abs(moveX) > that.moveCount ? -that.moveCount : moveX;
-                                util._transform(this, 'transform', 'translateX(' + x + 'px)');
-                            }
-                        }
-                    }
-                }, util._supportPassive() ? { passive: true } : false);
-
-                // 滑动结束
-                this.container[i].addEventListener('touchend', function () {
-                    // 防止手机滑动跨越了几个容器，造成没有开始位置
-                    if (moveStart === null) {
-                        return;
-                    }
-
-                    // 因为已经滑动一段距离了，把动画调快一点
-                    util._transform(this, 'transitionDuration', '125ms');
-
-                    // 已经显示按钮
-                    if (this.classList.contains('move-out-click')) {
-                        // 向右滑动
-                        if (moveX > 0) {
-                            // 超过位移系数一半就隐藏按钮
-                            //var x = moveX > (that.moveCount / 2) ? 0 : -moveCount;
-                            var x = moveX > 10 ? 0 : -that.moveCount;  // 改为超过10就隐藏按钮
-                            util._transform(this, 'transform', 'translateX(' + x + 'px)');
-                            if (x === 0) {
-                                this.classList.remove('move-out-click');
-                            }
-                        }
-                    } else {
-                        // 向左滑动
-                        if (moveX < 0) {
-                            // 超过位移系数一半就显示按钮
-                            var x = Math.abs(moveX) > that.moveCount / 2 ? -that.moveCount : 0;
-                            util._transform(this, 'transform', 'translateX(' + x + 'px)');
-                            if (x !== 0) {
-                                this.classList.add('move-out-click');
-                            }
-                        }
-                    }
-
-                    // 恢复初始化状态
-                    moveStart = null;
-                    moveX = 0;
-                    moveY = 0;
-                }, false);
-
-                // 滑动取消
-                this.container[i].addEventListener('touchcancel', function () {
-                    // 回到初始位置
-                    util._transform(this, 'transitionDuration', '225ms');
-                    util._transform(this, 'transform', 'translateX(0px)');
-                    this.classList.remove('move-out-click');
-                    // 恢复初始化状态
-                    moveStart = null;
-                    moveX = 0;
-                    moveY = 0;
-                }, false);
+                this._transform(this.container[i], 'transform', 'translateZ(0)');
+                this._bindEvent(this.container[i]);
             }
-        }
-    };
+        },
+        _start: function (el) {
+            //console.log(el);
+            // 还原动画时间
+            this._transform(el, 'transitionDuration', '0ms');
 
-    var util = {
+            // 关闭其他项的按钮，也可以放在滑动结束
+            for (var ii = 0; ii < this.container.length; ii++) {
+                if (this.container[ii].classList.contains('move-out-click') && this.container[ii] != el) {
+                    // 动画慢一点
+                    this._transform(this.container[ii], 'transitionDuration', '225ms');
+                    this._transform(this.container[ii], 'transform', 'translateX(0px)');
+                    this.container[ii].classList.remove('move-out-click');
+                }
+            }
+
+            this.moveX = 0;
+            this.moveY = 0;
+            // 记录滑动位置
+            this.moveStart = {
+                x: event.touches[0].pageX,
+                y: event.touches[0].pageY
+            }
+        },
+        _move: function (el) {
+            // 防止手机滑动跨越了几个容器，造成没有开始位置
+            if (this.moveStart === null) {
+                return;
+            }
+
+            var nowX = event.touches[0].pageX;
+            var nowY = event.touches[0].pageY;
+            this.moveX = nowX - this.moveStart.x;
+            this.moveY = nowY - this.moveStart.y;
+
+            // 左右滑动
+            if (Math.abs(this.moveX) > Math.abs(this.moveY)) {
+                // 向右滑动
+                if (this.moveX > 0) {
+                    // 有按钮的时候才处理
+                    if (el.classList.contains('move-out-click')) {
+                        var x = this.moveX > this.moveCount ? 0 : -this.moveCount + this.moveX;
+                        this._transform(el, 'transform', 'translateX(' + x + 'px)');
+                    }
+                }
+                // 向左滑动
+                if (this.moveX < 0) {
+                    // 没有按钮的时候才处理
+                    if (!(el.classList.contains('move-out-click'))) {
+                        var x = Math.abs(this.moveX) > this.moveCount ? -this.moveCount : this.moveX;
+                        this._transform(el, 'transform', 'translateX(' + x + 'px)');
+                    }
+                }
+            }
+        },
+        _end: function (el) {
+            // 防止手机滑动跨越了几个容器，造成没有开始位置
+            if (this.moveStart === null) {
+                return;
+            }
+
+            // 因为已经滑动一段距离了，把动画调快一点
+            this._transform(el, 'transitionDuration', '125ms');
+
+            // 已经显示按钮
+            if (el.classList.contains('move-out-click')) {
+                // 向右滑动
+                if (this.moveX > 0) {
+                    // 超过位移系数一半就隐藏按钮
+                    //var x = moveX > (this.moveCount / 2) ? 0 : -this.moveCount;
+                    var x = this.moveX > 10 ? 0 : -this.moveCount;  // 改为超过10就隐藏按钮
+                    this._transform(el, 'transform', 'translateX(' + x + 'px)');
+                    if (x === 0) {
+                        el.classList.remove('move-out-click');
+                    }
+                }
+            } else {
+                // 向左滑动
+                if (this.moveX < 0) {
+                    // 超过位移系数一半就显示按钮
+                    var x = Math.abs(this.moveX) > this.moveCount / 2 ? -this.moveCount : 0;
+                    this._transform(el, 'transform', 'translateX(' + x + 'px)');
+                    if (x !== 0) {
+                        el.classList.add('move-out-click');
+                    }
+                }
+            }
+
+            // 恢复初始化状态
+            this.moveStart = null;
+            this.moveX = 0;
+            this.moveY = 0;
+        },
+        _cancel: function (el) {
+            // 回到初始位置
+            this._transform(el, 'transitionDuration', '225ms');
+            this._transform(el, 'transform', 'translateX(0px)');
+            el.classList.remove('move-out-click');
+            // 恢复初始化状态
+            this.moveStart = null;
+            this.moveX = 0;
+            this.moveY = 0;
+        },
+        _bindEvent: function (item) {
+            this.start = this._start.bind(this, item);
+            this.move = this._move.bind(this, item);
+            this.end = this._end.bind(this, item);
+            this.cancel = this._cancel.bind(this, item);
+
+            item.addEventListener('touchstart', this.start,  false);
+            item.addEventListener('touchmove', this.move, this._supportPassive() ? { passive: true } : false);
+            item.addEventListener('touchend', this.end, false);
+            item.addEventListener('touchcancel', this.cancel, false);
+        },
         _supportPassive: function () {
             var support = false;
             try {
